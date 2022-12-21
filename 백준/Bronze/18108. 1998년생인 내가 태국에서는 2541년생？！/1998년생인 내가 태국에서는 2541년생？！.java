e5bf5321693c28bba1c05c5a9c6226a8953fd61e
Hwann <107896458+DSJHKIM@users.noreply.github.com>
2022-12-21 17:49:44 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 208 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int num2 = 2541 - 1998;
+        System.out.println(num - num2);
+        sc.close();
+    }
+}
